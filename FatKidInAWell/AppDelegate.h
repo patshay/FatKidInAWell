60412bddfd86c5188bf067843b3b19c7596ce193
patshay <patshay@gmail.com>
2013-03-07 18:10:07 -0800

    Initial Commit

---
+++ FatKidInAWell/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  FatKidInAWell
+//
+//  Created by Patrick Shay on 3/7/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
